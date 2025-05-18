2c1e00bbfc0a4574622ae8629d5b880adef7865a
BilouBrot <github@traut.men>
2025-05-18 16:53:52 +0200

    Fix robot ID and enhance collision detection logic in navigation

--- client.py
+++ client.py
@@ -6,11 +6,9 @@ from pipuck.pipuck import PiPuck
 # Define variables and callbacks
 Broker = "192.168.178.56"  # Replace with your broker address
 Port = 1883 # standard MQTT port
-pi_puck_id = '17'
+pi_puck_id = '16'
 x_bound = 2.0
 y_bound = 1.0
-x_pos = 0.0
-y_pos = 0.0
 
 puck_dict = {}
 new_message = True
@@ -55,6 +53,24 @@ def check_bounds(x, y, radius = 0.0):
         return False
     return True
 
+def collsion_detected(x, y, radius = 0.0):
+    # Check if the robot is within the bounds of the arena
+    if x < 0 + radius or x > x_bound - radius:
+        return True
+    if y < 0 + radius or y > y_bound - radius:
+        return True
+    # Check for collision with other robots
+    for key, value in puck_dict.items():
+        if key != pi_puck_id:
+            pos = value.get('position')
+            if pos:
+                other_x = pos[0]
+                other_y = pos[1]
+                distance = ((x - other_x) ** 2 + (y - other_y) ** 2) ** 0.5
+                if distance < radius:
+                    return True
+    return False
+
 def get_position():
     data = puck_dict.get(pi_puck_id)
     if data:
@@ -72,14 +88,21 @@ for i in range(5000):
     # TODO: Do your stuff here
     # Print the updated dictionary
     print(f"Updated puck_dict: {puck_dict}")
-    # if new_message:
-    #     # pipuck.epuck.set_inner_leds()
+    if new_message:
+        pipuck.epuck.set_inner_leds()
     # Get the current position of the robot
     x, y = get_position()
     print(f"Current position: {x}, {y}")
     # drive randomly
     if x is not None and y is not None:
-        if check_bounds(x, y, radius=0.05):
+        if collsion_detected(x, y, radius=0.05):
+            # turn to the left
+            pipuck.epuck.set_motor_speeds(-1000, 1000)
+            time.sleep(0.5)
+            # move forward
+            pipuck.epuck.set_motor_speeds(1000, 1000)
+            
+        else:
             if i % 100 == 0:
                 # turn to the left
                 pipuck.epuck.set_motor_speeds(-1000, 1000)
@@ -89,14 +112,6 @@ for i in range(5000):
                 pipuck.epuck.set_motor_speeds(1000, -1000)
                 time.sleep(0.5)
             pipuck.epuck.set_motor_speeds(1000, 1000)
-            
-        if not check_bounds(x, y, radius=0.05):
-            # turn to the right
-            pipuck.epuck.set_motor_speeds(1000, -1000)
-            time.sleep(0.5)
-            # move forward
-            pipuck.epuck.set_motor_speeds(1000, 1000)
-            time.sleep(0.5)
     else:
         # If no position data, stop the robot
         pipuck.epuck.set_motor_speeds(0, 0)
