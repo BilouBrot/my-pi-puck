01bba76a384ebbb88684be2bbf103930886dc7f1
BilouBrot <github@traut.men>
2025-05-19 11:47:37 +0200

    Fix typo in collision detection function call during movement

--- client.py
+++ client.py
@@ -121,7 +121,7 @@ for i in range(99999):
                 print(f"Current angle: {angle}")
             # move forward
             x, y, angle = get_position()
-            while collision_detected(x, y, radius=0.05)[0]:
+            while collsion_detected(x, y, radius=0.05)[0]:
                 pipuck.epuck.set_motor_speeds(speed, speed)
                 time.sleep(0.2)
                 x, y, angle = get_position()
